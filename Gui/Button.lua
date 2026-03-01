71aaa26e943273ef82a83eea28899734f9e1bd97
FZGecko <135158221+FZGecko@users.noreply.github.com>
2026-03-01 14:34:21 +0100

    Update Button.lua

--- Gui/Button.lua
+++ Gui/Button.lua
@@ -1,58 +1,55 @@
-return  function(import)
-    local Kernel = import("Core/Kernel")
-    local Janitor = import("Core/Janitor")
-    local ThemeApplier = import("Gui/ThemeApplier")
-    local Theme = import("Gui/Theme")
+return function(import)
+	local Kernel = import("Core/Kernel")
+	local Janitor = import("Core/Janitor")
+	local ThemeApplier = import("Gui/ThemeApplier")
+	local Theme = import("Gui/Theme")
 
-    local Button = {}
-    Button.__index = Button
+	local Button = {}
+	Button.__index = Button
 
-    function Button.Create(properties)
-        local elementJanitor = Janitor.new()
+	function Button.Create(properties)
+		local elementJanitor = Janitor.new()
 
-        local button = Kernel:Create("TextButton", {
-            Size = UDim2.new(1, 0, 0, 30),
-            BackgroundColor3 = Theme.Primary,
-            Font = Theme.Font,
-            TextSize = Theme.Font_Size,
-            TextColor3 = Theme.Text,
-            Text = properties.Text or "Button",
-            LayoutOrder = properties.LayoutOrder,
-            BorderSizePixel = 0
-        })
-        elementJanitor:Add(button)
-        elementJanitor:Add(Kernel:Create("UICorner", { CornerRadius = UDim.new(0, Theme.Rounding), Parent = button }))
+		local button = Kernel:Create("TextButton", {
+			Size = UDim2.new(1, 0, 0, 30),
+			BackgroundColor3 = Theme.Primary,
+			Font = Theme.Font,
+			TextSize = Theme.FontSize,
+			TextColor3 = Theme.Text,
+			Text = properties.Text or "Button",
+			LayoutOrder = properties.LayoutOrder,
+			BorderSizePixel = 0
+		})
+		elementJanitor:Add(button)
+		elementJanitor:Add(Kernel:Create("UICorner", { CornerRadius = UDim.new(0, Theme.Rounding), Parent = button }))
+		ThemeApplier:Apply(button)
 
-        local function SetState(state)
-            if state == "Hovered" then
-                button.BackgroundColor3 = Theme.Secondary
-            elseif state == "Pressed" then
-                button.BackgroundColor3 = Theme.Accent
-            else
-                button.BackgroundColor3 = Theme.Primary
-            end
-        end
+		local function SetState(state)
+			if state == "Hovered" then
+				button.BackgroundColor3 = Theme.Hover
+			elseif state == "Pressed" then
+				button.BackgroundColor3 = Theme.Accent
+			else
+				button.BackgroundColor3 = Theme.Primary
+			end
+		end
 
-        elementJanitor:Add(Kernel:Connect(button.MouseEnter, function() SetState("Hovered") end))
-        elementJanitor:Add(Kernel:Connect(button.MouseLeave, function() SetState("Normal") end))
-        elementJanitor:Add(Kernel:Connect(button.MouseButton1Down, function() SetState("Pressed") end))
-        elementJanitor:Add(Kernel:Connect(button.MouseButton1Up, function() SetState("Hovered") end))
+		elementJanitor:Add(Kernel:Connect(button.MouseEnter, function() SetState("Hovered") end))
+		elementJanitor:Add(Kernel:Connect(button.MouseLeave, function() SetState("Normal") end))
+		elementJanitor:Add(Kernel:Connect(button.MouseButton1Down, function() SetState("Pressed") end))
+		elementJanitor:Add(Kernel:Connect(button.MouseButton1Up, function() SetState("Hovered") end))
 
-        if properties.Callback then
-            elementJanitor:Add(Kernel:Connect(button.MouseButton1Click, properties.Callback))
-        end
+		if properties.Callback then
+			elementJanitor:Add(Kernel:Connect(button.MouseButton1Click, properties.Callback))
+		end
 
-        SetState("Normal")
+		SetState("Normal")
 
-        -- Give access to destroy for easy deletion
 		button.Destroy = function()
 			elementJanitor:Clean()
 		end
 
-        return button
-    end
-
-    return Button
+		return button
+	end
+	return Button
 end
-
--- Properties : Text, Callback, LayoutOrder
