3c6554ebf501aafbd561b099725afe433e5d008b
FZGecko <135158221+FZGecko@users.noreply.github.com>
2026-03-01 15:16:57 +0100

    Update Section.lua

--- Gui/Section.lua
+++ Gui/Section.lua
@@ -2,46 +2,84 @@ return function(import)
     local Kernel = import("Core/Kernel")
     local Janitor = import("Core/Janitor")
     local Theme = import("Gui/Theme")
+    local Services = import("Core/Services")
+    local UserInputService = Services.Get("UserInputService")
 
-    local Section = {}
-    Section.__index = Section
+    local Slider = {}
+    Slider.__index = Slider
 
-    function Section.Create(properties)
-        local sectionJanitor = Janitor.new()
+    function Slider.Create(properties)
+        local elementJanitor = Janitor.new()
 
-        local sectionFrame = Kernel:Create("Frame", {
-            Size = UDim2.new(1, 0, 0, 0), -- Auto-size in Y direction
-            BackgroundColor3 = Theme.Surface,
+        local sliderFrame = Kernel:Create("Frame", {
+            Size = UDim2.new(1, 0, 0, 20),
+            BackgroundColor3 = Theme.Primary,
             BorderSizePixel = 0,
-            LayoutOrder = properties.LayoutOrder or 0, --Let sections have layoutorder too
+            LayoutOrder = properties.LayoutOrder
         })
-        sectionJanitor:Add(sectionFrame)
-        sectionJanitor:Add(Kernel:Create("UICorner", { CornerRadius = UDim.new(0, Theme.Rounding), Parent = sectionFrame }))
+        elementJanitor:Add(sliderFrame)
+        elementJanitor:Add(Kernel:Create("UICorner", { CornerRadius = UDim.new(0, Theme.Rounding), Parent = sliderFrame }))
 
-        -- Add a UIListLayout to automatically arrange elements vertically
-        local listLayout = Kernel:Create("UIListLayout", {
-            FillDirection = Enum.FillDirection.Vertical,
-            SortOrder = Enum.SortOrder.LayoutOrder,
-            Padding = UDim.new(0, 2),
-            Parent = sectionFrame
+        local sliderBar = Kernel:Create("Frame", {
+            Size = UDim2.new(0.5, 0, 1, 0),
+            BackgroundColor3 = Theme.Accent,
+            BorderSizePixel = 0,
+            Parent = sliderFrame,
         })
-        sectionJanitor:Add(listLayout)
+        elementJanitor:Add(Kernel:Create("UICorner", { CornerRadius = UDim2.new(1, Theme.Rounding, 1, Theme.Rounding), Parent = sliderBar }))
+
+        local thumb = Kernel:Create("Frame", {
+            Size = UDim2.fromOffset(16, 20),
+            Position = UDim2.new(1,0,0,0),
+            BackgroundColor3 = Theme.Text,
+            BorderSizePixel = 0,
+            Parent = sliderBar
+        })
+        elementJanitor:Add(Kernel:Create("UICorner", { CornerRadius = UDim2.new(1, Theme.Rounding, 1, Theme.Rounding), Parent = thumb }))
+        
+        local dragging = false
+        local dragStart
+        local sliderStart
+
+        elementJanitor:Add(Kernel:Connect(thumb.InputBegan, function(input)
+            if input.UserInputType == Enum.UserInputType.MouseButton1 then
+                dragging = true
+                dragStart = UserInputService:GetMouseLocation()
+                sliderStart = sliderBar.Size
+            end
+        end))
+
+        elementJanitor:Add(Kernel:Connect(thumb.InputEnded, function(input)
+            if input.UserInputType == Enum.UserInputType.MouseButton1 then
+                dragging = false
+            end
+        end))
+
+        LoopManager:Bind("SliderDrag", "Render", function()
+            if dragging then
+                local delta = (UserInputService:GetMouseLocation().X - dragStart.X)
+                local newSizeX = math.clamp(sliderStart.X.Offset + delta, 0, sliderFrame.AbsoluteSize.X)
+                sliderBar.Size = UDim2.new(0, newSizeX, 1, 0)
+                properties.Callback(newSizeX/sliderFrame.AbsoluteSize.X)
+            end
+        end)
 
         -- Give access to destroy for easy deletion
-		sectionFrame.Destroy = function()
-			sectionJanitor:Clean()
+		sliderFrame.Destroy = function()
+			elementJanitor:Clean()
 		end
 
-        return sectionFrame
+        return sliderFrame
     end
 
-    return Section
+    return Slider
 end
 
--- Properties:
---  LayoutOrder: (Optional) The order in which this section appears.
---  ZIndex (Optional) - The ZIndex of this Section
---  Visible (Optional) - If the Section is visible, this will require a Hook; Default is True and Visible
+-- Properties : Callback, LayoutOrder
 
--- Methods:
--- Destroy() - Destroys the section and all its children.
+-- Callback returns a number between 0 and 1 representing the slider value.
+
+-- TODO: Add a Value property and setter to programatically change the slider's position.
+--       Add MouseEnter/Leave highlights.
+--       Add Keyboard support.
+--       Consider making the thumb a separate image.
